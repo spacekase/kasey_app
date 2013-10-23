65e3c242c68b77763d48e4691d4e2f781280d22c
kasey lundquist <klundquist2014@kellogg.northwestern.edu>
2013-10-23 15:17:09 -0500

    rails new

---
+++ kasey_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KaseyApp::Application.config.secret_key_base = 'df3814119f4281184bf75bad2a637af962c6793b2e3f2a7c542032984d94e9fbbee4c9779d9eddad8348f5c8a6923bbc1c6214ec9810f441ed161947361cb14a'
