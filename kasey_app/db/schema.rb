7360fcac17ee71490de84fb8fed1d669abcd31a2
kasey lundquist <klundquist2014@kellogg.northwestern.edu>
2013-10-31 13:26:12 -0500

    added table "User"

--- kasey_app/db/schema.rb
+++ kasey_app/db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131030195357) do
+ActiveRecord::Schema.define(version: 20131031181633) do
 
   create_table "items", force: true do |t|
     t.string   "name"
     t.string   "description"
     t.float    "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
